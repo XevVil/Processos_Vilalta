619af81159918f4ac1856af6a9e4181859cb8b82
xavier <xvilalta96@gmail.com>
2019-10-08 18:48:14 +0200

    AAAA

---
+++ Practica1_XavierVilalta/equacio2.py
@@ -0,0 +1,9 @@
+from Equacio1 import EquacioPrimerGrau
+
+
+eq = EquacioPrimerGrau("20x + 30 = 70")
+eq.calcula()
+eq = EquacioPrimerGrau("20x - 70 = -30")
+eq.calcula()
+eq = EquacioPrimerGrau("3.4x - 7.8 = 4.3")
+eq.calcula()
